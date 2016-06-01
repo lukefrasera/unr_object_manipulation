690df59f42aa2ad7af21721d5185af350aa1a13c
"Luke Fraser" <lukefrasera@gmail.com>
2016-06-01 16:15:27 -0700

    adding new files to the repo

--- include/grasp_server_library/grasp_server.h
+++ include/grasp_server_library/grasp_server.h
@@ -1,24 +1,76 @@
-/*Copyright 2016 Luke Fraser
+/*
+Copyright 2016 Luke Fraser
 
-  This file is part of UNR_Object_Manipulation.
+This file is part of UNR_Object_Manipulation.
 
-  UNR_Object_Manipulation is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+UNR_Object_Manipulation is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-  UNR_Object_Manipulation is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+UNR_Object_Manipulation is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with UNR_Object_Manipulation.  If not, see <http://www.gnu.org/licenses/>. 
+You should have received a copy of the GNU General Public License
+along with UNR_Object_Manipulation.  If not, see <http://www.gnu.org/licenses/>. 
 */
 #ifndef INCLUDE_GRASP_SERVER_LIBRARY_GRASP_SERVER_H_
 #define INCLUDE_GRASP_SERVER_LIBRARY_GRASP_SERVER_H_
+#include <string>
+#include <unordered_map>
+#include "moveit_msgs/Grasp.h"
+#include "unr_object_manipulation/uomconfig.h"
+#include "ros/ros.h"
+// #include "uomconfig.h"
 
 namespace grasplib {
+class Grasp {
+ public:
+  Grasp();
+  ~Grasp();
+ private:
+  moveit_msgs::Grasp grasp_;
+};
+////////////////////////////////////////////////////////////////////////////////
+// Class:
+//   ObjectPickPlace
+// Description:
+//   This class stores all grasp and object information needed to produce a full
+//   pick and place of the object. It will store the pick grasp as well as the
+//   place grasp.
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+class ObjectPickPlace {
+ public:
+  ObjectPickPlace();
+  ~ObjectPickPlace();
 
+ private:
+  std::string name;
+  Grasp pick;
+  Grasp place;
+};
+
+class GraspServer {
+ public:
+  GraspServer(std::string arm);
+  ~GraspServer();
+
+  // Public Facing API functions
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Status Functions
+  //////////////////////////////////////////////////////////////////////////////
+  bool CheckServerState(std::string *status);
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Grasp Functions
+  //////////////////////////////////////////////////////////////////////////////
+ private:
+  ros::NodeHandle nh_;
+  std::unordered_map<std::string, ObjectPickPlace> objects_;
+};
 }  // namespace grasplib
 #endif  // INCLUDE_GRASP_SERVER_LIBRARY_GRASP_SERVER_H_
